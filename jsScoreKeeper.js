c6cb57741dcb9be14039ad958fb9110ac967870d
George Higbie <gehigbie@gmail.com>
2017-01-17 20:09:40 -0700

    added content to js file

--- jsScoreKeeper.js
+++ jsScoreKeeper.js
@@ -1,2 +1,14 @@
 //this file was created in the command line
 //alert("JS file is connected");
+
+var maxScore = 0;
+var player1Score = 0;
+var player2Score = 0;
+var hasWon = false;
+
+var button1 = document.querySelector("#playerOne");
+var button2 = document.querySelector("#playerTwo");
+var buttonR = document.querySelector("button")[2];
+
+
+
