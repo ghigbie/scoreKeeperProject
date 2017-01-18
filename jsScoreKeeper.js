f00622943d99e498aa1708a82d95eda4ac45751e
George Higbie <gehigbie@gmail.com>
2017-01-17 20:22:59 -0700

    added more javascript

--- jsScoreKeeper.js
+++ jsScoreKeeper.js
@@ -6,9 +6,46 @@ var player1Score = 0;
 var player2Score = 0;
 var hasWon = false;
 
+
 var button1 = document.querySelector("#playerOne");
 var button2 = document.querySelector("#playerTwo");
 var buttonR = document.querySelector("button")[2];
+var score1 = document.querySelector("#score1");
+var score2 = document.querySelector("#score2");
+
+button1.addEventListener("click", function(){
+	score1.textContent = playerOne++;
+	checkWin();
+});
+
+button2.addEventListener("click", function(){
+	if(player)
+	score2.textContent = playerTwo++;
+	checkWin();
+});
+
+function checkWin(){
+	if(player1Score > player2Score){
+		player1Wins = true;
+	}
+
+}
+
+function player1Wins(){
+	alert("Player One Wins");
+	setInterval(function(){
+		//make background flash to playerone color
+	}, 200);
+}
+
+function player2Wins(){
+	alert("Palyer Two Wins")
+	setInterval(function(){
+		//make background flash to playertwo color
+	}, 200);
+}
+
+
 
 
 
